69acda19996543e6553da8008d0b851248a23ecf
LuisaFerro <luisaferro9@gmail.com>
2022-12-01 11:44:18 -0300

    semana 2  - exercícios

---
+++ Questionário 4/estudo recursão.py
@@ -0,0 +1,59 @@
+def fatorial(n):
+    if n == 0: #caso base
+        return 1
+    else:
+        return n*fatorial(n-1)
+
+def soma(lista):
+    if len(lista) == 1:
+        return lista[0]
+    else:
+        return lista[0]+soma(lista[1:])
+
+#------------------------- Lista [simplesmente] Encadeada! ----------------------
+#lista circular: retorna ao primeiro, mas como saber quem é o primeiro?
+#lista duplamente encadeada: como implementar?
+
+#> inserir no começo, meio ou final OU recursivamente sempre inserir no começo, considerando listas menores
+
+class Item():
+    def __init__(self,dado, prox=None): #se for o último elemento, já sabe-se que o próximo é NONE
+        self.dado = dado
+        self.prox = prox
+
+
+def insere_inicio(lista,item):# não precisa de self pois não é um método da classe
+    if lista != None: #quando é None,return False
+        item.prox = lista #aqui define-se o novo primeiro elemento; assim fica atribuido ao parametro "prox" de joao -> paulo (que já estava na estrutura anteriormente)
+    return item #retorna o endereço do último item adicionado, a nova cabeça
+
+def insere(lista,item,pos):
+    if pos == 0: 
+        return insere_inicio(lista,item)
+    
+    if lista:
+        lista.prox = insere(lista.prox,item,pos-1)
+        return lista
+    
+    raise ValueError('Posição inválida') #pois o código funciona caso coloque um valor de append válido. Se não, dará erro
+
+    
+        
+
+# item = Item(1)
+# item2 = Item(2,item)
+
+lista = None 
+#lista não é uma estrutura [], mas um objeto que liga objetos da classe item() e armazena  a memória dos últimos ítens adicionados
+
+lista = insere_inicio(lista,Item('Paulo'))
+lista = insere_inicio(lista,Item('Joao')) #insere_lista deve retornar a cabeça da lista para que o próximo elemento adicionado possa se basear na atual cabeça como elemento qu aponta
+
+lista = insere(lista,Item('Caetano'),0)
+
+lista = insere(lista,Item('Caetano'),30)
+
+print('oi')
+
+
+
---
+++ Questionário 4/readme.md
@@ -0,0 +1,7 @@
+segundo o livro da disciplina, *Sempre comece a implementar o código da sua função recursiva pelo teste do caso base (condição de parada)*
+
+**leis da recursividade**
+1. Um algoritmo recursivo deve possuir um caso base (base case); 2. Um algoritmo recursivo deve modificar o seu estado e se aproximar do caso base; 3. Um algoritmo recursivo deve chamar a si mesmo, recursivamente.
+
+**Aplicação de recursividade**
+> Lista encadeada tem a ver com a alocação dinâmica de memória; É necessário saber o ítem e seu sucesso
